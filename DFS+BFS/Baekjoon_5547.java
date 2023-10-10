3062e8ef4005eb4a6783b2789595861cd96dee7b
Yang-soeun <soeun8636@naver.com>
2023-10-10 18:29:25 +0900

    일루미네이션 - 수정했음

--- DFS+BFS/Baekjoon_5547.java
+++ DFS+BFS/Baekjoon_5547.java
@@ -11,86 +11,85 @@ import java.util.StringTokenizer;
 public class Baekjoon_5547 {
     static int W, H;
     static int[][] array;
+    static int[][] map;
     static boolean[][] visited;
-    static int[] arrX = {-1, -1, -1, 0, 0, 1, 1, 1};
-    static int[] arrY = {-1, 0, 1, -1, 1, -1, 0, 1};
-    static int count = 0;
-    static int near = 0;
-    static int newX, newY;
-
-    static class Pos{
+    static int[][] odd =  {{0, -1}, { -1,  0}, {0, 1}, {1, 1}, {1,  0}, {-1, 1}};;//홀수
+    static int[][] even = {{0, -1}, { -1, -1}, {0, 1}, {1, 0}, {1, -1}, {-1, 0}};//짝수
+    public static class Pos{
         int x;
         int y;
+
         public Pos(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-    public static int bfs(int x, int y){
-        count = 0;
 
+    public static void bfs(int x, int y){
         Queue<Pos> queue = new LinkedList<>();
         queue.add(new Pos(x, y));
 
         while(!queue.isEmpty()){
             Pos poll = queue.poll();
+            int newX = 0;
+            int newY = 0;
 
-            for(int i = 0; i<8; i++){
-                if((poll.x)%2 == 0){//짝수인 경우
-                    if(arrX[i]== 1 && arrY[i] ==-1 ||(arrX[i]== -1 && arrY[i] ==-1)) continue;
-                    newX = poll.x + arrX[i];
-                    newY = poll.y + arrY[i];
+            for(int i = 0; i<6; i++){
+                if((poll.x%2) == 0){//짝수인 경우
+                    newX = poll.x + even[i][0];
+                    newY = poll.y + even[i][1];
+                }else{//홀수인 경우
+                    newX = poll.x + odd[i][0];
+                    newY = poll.y + odd[i][1];
                 }
-                else{//홀수인 경우
-                    if((arrX[i]== 1 && arrY[i] ==1) ||(arrX[i]== -1 && arrY[i] ==1)) continue;
-                    newX = poll.x + arrX[i];
-                    newY = poll.y + arrY[i];
-                }
- 
 
-                if(newX <0 ||newY<0 ||newX>=H || newY >=W) continue;
+                if(newX < 0 || newY <0|| newX >= H+2 || newY >= W+2) continue;
+
                 if(array[newX][newY] == 1){
-                    near ++;
-                    if(visited[newX][newY] == false) {
-                        queue.add(new Pos(newX, newY));
-                        visited[newX][newY] = true;
-                    }
+                    map[poll.x][poll.y] ++;
+                    continue;
                 }
+
+                if(visited[newX][newY] || map[newX][newY] != 0) continue;
+
+                visited[newX][newY] = true;
+                queue.add(new Pos(newX, newY));
             }
-
-            count += (6-near);
-            near = 0;
         }
-
-        return count;
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        W = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-        int len = 0;
+        int count = 0;
 
-        array = new int[H][W];
-        visited = new boolean[H][W];
+        W = Integer.parseInt(st.nextToken());//열
+        H = Integer.parseInt(st.nextToken());//행
 
-        for(int i = 0; i<H; i++){//행
+        array = new int[H+2][W+2];
+        map = new int[H+2][W+2];
+        visited = new boolean[H+2][W+2];
+
+        for(int i =1; i<= H; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<W; j++){//열
+            for(int j = 1; j<=W; j++){
                 array[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        for(int i = 0; i<H; i++){
-            for(int j = 0; j<W; j++){
-                if(array[i][j] == 1 && visited[i][j]==false){
+                if(array[i][j] == 1)
                     visited[i][j] = true;
-                    len += bfs(i, j);
-                }
             }
         }
 
-        System.out.println(len);
+        bfs(0, 0);
+        visited[0][0] = true;
+
+        for(int i =0; i<=H+1; i++){
+            for(int j = 0; j<=W+1; j++){
+                count += map[i][j];
+            }
+        }
+
+        System.out.println(count);
+
     }
 }
