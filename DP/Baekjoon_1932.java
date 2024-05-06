bfa6272c19355df5a9df712068ef9b6d6950ca71
Yang-soeun <soeun8636@naver.com>
2024-05-06 14:10:08 +0900

    정수 삼각형

--- DP/Baekjoon_1932.java
+++ DP/Baekjoon_1932.java
@@ -18,40 +18,24 @@ public class Baekjoon_1932 {
 
         int N = Integer.parseInt(br.readLine());
         int array[][] = new int[N][N];
-        int dp[][] =  new int[N][N];
-        boolean visited[][] = new boolean[N][N];
+        int dp[][] =  new int[N+1][N+1];
 
-        array[0][0] = dp[0][0] = Integer.parseInt(br.readLine());
-        visited[0][0] = true;
+        array[0][0] = dp[1][1] = Integer.parseInt(br.readLine());
 
         for(int i = 1; i<N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j<i+1; j++){
                 array[i][j] = Integer.parseInt(st.nextToken());
-                visited[i][j] = true;
             }
         }
 
-        dp[1][0] = dp[0][0] + array[1][0];
-        dp[1][1] = dp[0][0] + array[1][1];
-
-        for(int i = 2; i<N; i++){
-            for(int j = 0; j<array[i].length; j++){
-                if(!visited[i][j])
-                    continue;
-                if(j == 0){
-                    dp[i][j] = Math.max(dp[i-1][j] + array[i][j], dp[i-1][j] + array[i][j+1]);
-                }
-                else if(j == N-1 || !visited[i][j+1]){
-                    dp[i][j] = dp[i-1][j-1] + array[i][j];
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j-1] + array[i][j], dp[i-1][j] + array[i][j]);
-                }
+        for(int i = 2; i<=N; i++){
+            for(int j = 1; j<=N; j++){
+                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + array[i-1][j-1];
             }
         }
 
-        int max = Arrays.stream(dp[N-1]).max().getAsInt();
+        int max = Arrays.stream(dp[N]).max().getAsInt();
         System.out.println(max);
     }
 }
