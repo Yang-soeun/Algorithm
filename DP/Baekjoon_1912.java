aaf0ff63ab71296e743ec58a66321722e380b6c6
Yang-soeun <soeun8636@naver.com>
2024-05-22 17:52:57 +0900

    연속 합

---
+++ DP/Baekjoon_1912.java
@@ -0,0 +1,32 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 연속 합
+ */
+public class Baekjoon_1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = array[0];
+        for(int i = 1; i< n; i++){
+            dp[i] = Math.max(dp[i-1]+ array[i], array[i]);
+        }
+
+        int max = Arrays.stream(dp).max().getAsInt();
+        System.out.println(max);
+    }
+}
