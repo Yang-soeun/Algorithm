22e4f9462c57ac9a567c87158de826b9672e81cd
Yang-soeun <soeun8636@naver.com>
2024-06-13 08:58:52 +0900

    정수론을 적용한 방식 추가

--- implementation/programmers_숫자의 표현.java
+++ implementation/programmers_숫자의 표현.java
@@ -16,3 +16,18 @@ class Solution {
         return answer;
     }
 }
+
+//숫자의 연속된 자연수의 합은 주어진 숫자의 홀수의 약수 개수와 같다 - 정수론
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 1; i<=n; i+=2){
+            if(n%i == 0){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
