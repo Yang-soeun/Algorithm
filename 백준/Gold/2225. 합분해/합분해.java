1353b843379601412ca32ee856b5944a82b60440
Yang-soeun <soeun8636@naver.com>
2024-08-21 20:22:24 +0900

    [Gold V] Title: 합분해, Time: 100 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 합분해
+ */
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		int[][] dp = new int[k+1][n+1];
+
+		Arrays.fill(dp[1], 1);
+
+		for(int i = 1; i<=k; i++){
+			dp[i][0] = 1;
+		}
+
+		for(int i = 1; i<=k; i++){
+			for(int j = 1; j<=n; j++){
+				dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1000_000_000;
+			}
+		}
+
+		System.out.println(dp[k][n]);
+	}
+}
