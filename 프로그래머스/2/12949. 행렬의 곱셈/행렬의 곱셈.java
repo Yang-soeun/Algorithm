dbb16fc7966aef0cfcc49f913f105fcd7a3e0595
Yang-soeun <soeun8636@naver.com>
2024-08-30 09:36:51 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.79 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -4,15 +4,11 @@ class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr2[0].length];
         
-        int sum = 0;
-        
         for(int i = 0; i<arr2[0].length; i++){
             for(int j = 0; j<arr1.length; j++){
                 for(int k = 0; k<arr1[0].length; k++){
-                    sum += arr1[j][k] * arr2[k][i];
+                    answer[j][i] += arr1[j][k] * arr2[k][i];
                 }
-                answer[j][i] = sum;
-                sum = 0;
             }
         }
 
